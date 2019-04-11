1fc05c26e179594e946c2a30da634ed7ba024fd0
Akash Mitra <akashmitra@gmail.com>
2019-04-12 00:27:23 +0800

    wip

--- app/Http/Controllers/RepositoryController.php
+++ app/Http/Controllers/RepositoryController.php
@@ -10,20 +10,38 @@ class RepositoryController extends Controller
     {
     	return [
 	    		[
-	    			"id" => '1234567890',
+	    			"id" => '31415926',
 	    			"name" => "Kayna - Home", 
 	    			"type" => "home",
 	    			"description" => "Kayna is the default template for BlogTheory. This is a simple and elegant home template optimized for general blogging sites.", 
-	    			"url" => "https://example.org/1",
+	    			"url" => "https://gist.githubusercontent.com/akash-mitra/a8ad834eb955486ee594067a5d18c977/raw/dedbdcf2adbfde1ed4801ff21fb71ac079bfbd28/kayna-home.blade.php",
 	    			"released_at" => now(),
 	    			"version" => 1.0
 	    		],
 	    		[
-	    			"id" => '1234567891',
+	    			"id" => '31415927',
 	    			"name" => "Kayna - Page", 
-	    			"type" => "Kayna is the default template for BlogTheory. This is a clean looking page template optimized for simplicity, speed and ease of use.",
-	    			"description" => "test2 description", 
-	    			"url" => "https://gist.githubusercontent.com/akash-mitra/119fd129012d569b6e3ad1b2a6b38dea/raw/fd5b616b4e4138b4220556cd32bcde157ba2c31d/kayna-page.html",
+	    			"type" => "page",
+	    			"description" => "Kayna is the default template for BlogTheory. This is a clean looking page template optimized for simplicity, speed and ease of use.", 
+	    			"url" => "https://gist.githubusercontent.com/akash-mitra/a8ad834eb955486ee594067a5d18c977/raw/dedbdcf2adbfde1ed4801ff21fb71ac079bfbd28/kayna-page.blade.php",
+	    			"released_at" => now(),
+	    			"version" => 1.0
+	    		],
+	    		[
+	    			"id" => '31415928',
+	    			"name" => "Kayna - Category", 
+	    			"type" => "category",
+	    			"description" => "Kayna is the default template for BlogTheory. This is a clean looking Category template optimized for simplicity, speed and ease of use.", 
+	    			"url" => "https://gist.githubusercontent.com/akash-mitra/a8ad834eb955486ee594067a5d18c977/raw/dedbdcf2adbfde1ed4801ff21fb71ac079bfbd28/kayna-category.blade.php",
+	    			"released_at" => now(),
+	    			"version" => 1.0
+	    		],
+	    		[
+	    			"id" => '31415929',
+	    			"name" => "Kayna - Profile", 
+	    			"type" => "profile",
+	    			"description" => "Kayna is the default template for BlogTheory. This is a clean looking Profile template.", 
+	    			"url" => "https://gist.githubusercontent.com/akash-mitra/a8ad834eb955486ee594067a5d18c977/raw/dedbdcf2adbfde1ed4801ff21fb71ac079bfbd28/kayna-profile.blade.php",
 	    			"released_at" => now(),
 	    			"version" => 1.0
 	    		]
